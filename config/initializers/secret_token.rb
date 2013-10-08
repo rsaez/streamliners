591f96f4e03cb63b6acbece16d54d2dbc862a726
Nishant <nshah99@gatech.edu>
2013-10-08 18:44:12 -0400

    Initial repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Streamliners::Application.config.secret_key_base = '7dfc97fbe2b13d301e8351f3854f683ca9d494605fac41b8813d3781e4975d28f7eccdf90da6c021e913b85e24d7947e337cb5f1c11233ead2264fc9f8efc380'
