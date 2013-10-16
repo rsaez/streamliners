03ed2d2992f6a23d23e15c9b79727fd7411b9235
Harshit Gupta <hgupta2711@gmail.com>
2013-10-15 20:23:53 -0400

    child form 3 physical exam

---
+++ test/helpers/physical_exam_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class PhysicalExamHelperTest < ActionView::TestCase
+end
---
+++ test/models/physical_exam_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PhysicalExamTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
