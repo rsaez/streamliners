7038e289f59664592648e1a5e7e938d56fa7d80e
Nishant <nshah99@gatech.edu>
2013-11-26 21:27:50 -0500

    Basic validations on hemoglobin and order of tests. It is buggy and does not allow the user to update/create a field which is violating the validation. We would want to be able to skip validations for a few of them. Look at http://stackoverflow.com/questions/10435909/a-more-elegant-way-to-skip-validations-in-rails for skipping validations. Also, look at client side validations instead of model based validations. Can use jQuery to do this.

--- app/models/health_record.rb
+++ app/models/health_record.rb
@@ -1,3 +1,38 @@
 class HealthRecord < ActiveRecord::Base
-  belongs_to :children
+  belongs_to :child
+  validate :hemoglobin_validation, if: :is_hemoglobin_taken
+  validate :height_weight_for_blood_tests, if: :blood_being_drawn
+  
+
+  def  height_weight_for_blood_tests
+    errors.add(:weight,"Measure Height and Weight before doing this test") if !is_height_weight_taken
+  end
+
+  def is_hemoglobin_taken
+    return !hemoglobin.nil?
+  end
+  
+  def blood_being_drawn 
+    return is_hemoglobin_taken
+  end
+
+  def is_height_weight_taken
+    return !height_feet.nil? && !weight.nil?
+  end
+
+  def hemoglobin_validation
+    errors.add(:hemoglobin,"is too high. Must be less than 18. Contact Authorities") if hemoglobin >= 18
+    
+    if self.child.age >=2 && self.child.age <=5
+      errors.add(:hemoglobin,"Abnormal value for Hemoglobin. Contact Authorities.") if hemoglobin >= 18 || hemoglobin < 11.3
+    else
+      errors.add(:hemoglobin,"Abnormal value for Hemoglobin. Contact Authorities.") if hemoglobin >=18 || hemoglobin < 12  
+    end
+
+    if  self.child.gender == "Male"
+      errors.add(:hemoglobin,"Abnormal value for Hemoglobin. Contact Authorities.") if hemoglobin >=18 || hemoglobin < 12.3  
+    else 
+      errors.add(:hemoglobin,"Abnormal value for Hemoglobin. Contact Authorities.") if hemoglobin >=18 || hemoglobin < 11.8
+    end  
+  end
 end
